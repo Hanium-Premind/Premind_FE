77b44d5f64d25357bbcef3101c7cb2105e61cb6f
yengeun <124172198+yengeun@users.noreply.github.com>
2025-07-15 15:48:03 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
+import './assets/sass/common.scss';
 import Navbar from './pages/Navbar';
-//import Home from './pages/Home';
+import Home from './pages/Home';
+
 // import Interview from './pages/Interview/InterviewMain';
 // import InterviewRecords from './pages/Interview/InterviewRecords';
 // import Portfolio from './pages/Portfolio';
@@ -11,20 +11,23 @@ import Navbar from './pages/Navbar';
 // import Login from './pages/signInUp/Login';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                {/* Main page Route */}
-                <Route path="/" element={
-                    <>
-                        <Navbar />
+  return (
+    <Router>
+      <Navbar />
 
-                    </>
-                } />
-                
-            </Routes>
-        </Router>
-    );
-  }
+      <Routes>
+        {/* ✅ 각 페이지 라우팅 */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/interview" element={<Interview />} /> */}
+        {/* <Route path="/interview-records" element={<InterviewRecords />} /> */}
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
+        {/* <Route path="/mypage" element={<MyPage />} /> */}
+        {/* <Route path="/signup" element={<Signup />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
