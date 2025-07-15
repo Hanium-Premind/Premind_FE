5afbdb56159422c453e511ee58339d08a832b8dc
yengeun <7613ez@gmail.com>
2025-07-15 21:56:50 +0900

    chore: interview app.js 세팅

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './assets/sass/common.scss';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
-
-// import Interview from './pages/Interview/InterviewMain';
+import Interview from './pages/Interview/InterviewMain';
+import InterviewPractice from './pages/Interview/InterviewPrac';
 // import InterviewRecords from './pages/Interview/InterviewRecords';
 // import Portfolio from './pages/Portfolio';
 // import MyPage from './pages/MyPage';
@@ -18,8 +18,8 @@ const App = () => {
       <Routes>
         {/* ✅ 각 페이지 라우팅 */}
         <Route path="/" element={<Home />} />
-        {/* <Route path="/interview" element={<Interview />} /> */}
-        {/* <Route path="/interview-records" element={<InterviewRecords />} /> */}
+        <Route path="/interview" element={<Interview />} />
+        <Route path="/interview-practice" element={<InterviewPractice />} />
         {/* <Route path="/portfolio" element={<Portfolio />} /> */}
         {/* <Route path="/mypage" element={<MyPage />} /> */}
         {/* <Route path="/signup" element={<Signup />} /> */}
