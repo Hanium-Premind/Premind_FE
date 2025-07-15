367dbbc6fee3e80fd4d8de794ed65557a9b767e0
yengeun <7613ez@gmail.com>
2025-07-15 15:14:08 +0900

    feat:메인 슬라이드 이미지 제외 완성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import '../assets/sass/main.scss'; 
+import content1 from '../assets/img/content1.svg';
+import line from '../assets/img/line.png';
+import lb from '../assets/img/logoblk.svg';
+import S1 from '../assets/img/service1.svg';
+
+export default function MainPage() {
+  return (
+    <div className="main-page">
+      <section className="section hero">
+        <div className="hero-text">
+          <button>지금 바로 면접 연습하기</button>
+        </div>
+        <div className="hero-image-wrapper">
+            <img src={content1} alt="면접 분석 리포트"className="hero-image"/>
+        </div>
+      </section>
+
+     
+      <section className="section problem">
+        <div className="problem-text">
+          <p>
+            “면접 대비를 편하게 하고 싶어요”<br />
+            “면접 학원 비용이 부담돼요”<br />
+            “AI를 말하듯이 송출하는 툴이 많이 들어요”<br /><br />
+            이러한 걱정의 부담을 덜어드릴<br />
+            AI 아바타 기반 면접 서비스<br /><br />
+            취업준비생을 위한 면접 대비 플랫폼 서비스
+          </p>
+        </div>
+      </section>
+
+    <section className="section slogan">
+            <div className="slogan-background">
+                <img src={line} alt="라인 배경" className="bg-line" />
+                <img src={lb} alt="PREMIND 로고" className="bg-logo" />
+            </div>
+
+            <div className="slogan-overlay">
+                <p>
+                    당신이<br/>
+                    최종 합격에 도달할 때까지</p>
+                    <p><br/><br/>
+                    옆에서 함께 도와주는</p>
+            <button className="start-btn">지금 바로 시작해보기</button>
+            </div>
+    </section>    
+    <section className="section last">
+        <p>1. 면접 모드별 선택</p>
+    </section>    
+    </div>
+  );
+}
--- src/pages/signInUp/Login.jsx
+++ src/pages/signInUp/Login.jsx
@@ -1,5 +1,3 @@
-// src/pages/Login.js
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Navbar'; 
 
